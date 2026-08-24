969b8c994c9fde2c3743367434558a8847a758b8
Yuriy <yuriy@yuriybevov.ru>
2026-08-24 16:52:45 +0300

    Add structured product data: 38 products with images, specs, and SEO

--- pen.dev/newmark/src/data/catalog/products.js
+++ pen.dev/newmark/src/data/catalog/products.js
@@ -1,1 +1,14 @@
-export const products = [];
+import { adhesiveProducts } from "./products-adhesive.js";
+import { strappingProducts } from "./products-strapping.js";
+import { specialProducts } from "./products-special.js";
+import { stretchProducts, machineProducts } from "./products-other.js";
+import { miscProducts } from "./products-misc.js";
+
+export const products = [
+  ...adhesiveProducts,
+  ...strappingProducts,
+  ...specialProducts,
+  ...stretchProducts,
+  ...machineProducts,
+  ...miscProducts,
+];
